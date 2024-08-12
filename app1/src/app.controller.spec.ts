12d191e8524e86ab5e56fd143d4b0605804a2a47
PC <crunchydeer30@gmail.com>
2024-08-12 12:18:22 +0300

    make changes to app1

---
+++ app1/src/app.controller.spec.ts
@@ -0,0 +1,20 @@
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let appController: AppController;
+  let appService: AppService;
+
+  beforeEach(() => {
+    appService = new AppService();
+    appController = new AppController(appService);
+  });
+
+  describe('App1', () => {
+    it('should return "App1"', () => {
+      expect(appController.getHello()).toEqual({
+        message: 'App1',
+      });
+    });
+  });
+});
